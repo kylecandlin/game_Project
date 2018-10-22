07100f7ebead9e05ded215174dd65dc59580a44e
TobyParsons <TobyParsons@users.noreply.github.com>
2018-10-22 15:00:50 +0100

    Initial Upload
    Game file upload

---
+++ Assets/Player_Basic_Move.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Basic_Move : MonoBehaviour
+{
+
+    // Foreign Scripts
+    public Player_Stats Player_Stats;
+    public float Player_Stats_CurrentStamina;
+
+    public float moveSpeed;
+    public float walkSpeed;
+    public float runSpeedMultiplier;
+
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    public float jumpSpeed;
+  
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+        moveSpeed = walkSpeed;
+        jumpSpeed = 30;
+        walkSpeed = 4f;
+        runSpeedMultiplier = 2f;
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void Update()
+    {
+        Player_Stats = this.GetComponent<Player_Stats>();
+        Player_Stats_CurrentStamina = Player_Stats.currentStamina;
+        Controls();
+        Player_Stats.Regeneration(2, 10, 0, true);
+        Walk();
+    }
+
+    void Walk()
+    {
+        float y = 0;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            y += jumpSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && Player_Stats_CurrentStamina > 0f)
+        {
+            Player_Stats.AlterStats(0, -1, 0);
+            moveSpeed = walkSpeed * runSpeedMultiplier;
+        }
+        else
+        {
+            moveSpeed = walkSpeed;
+        }
+     
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+    
+        Vector2 movement = new Vector2(moveHorizontal * moveSpeed, y);
+
+    
+        rb2d.position += movement * Time.deltaTime;
+
+    }
+    void Controls()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Player_Stats.AlterStats(-10, 0, 0);
+        }
+
+    }
+}
---
+++ Assets/Player_Stats.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Player_Stats : MonoBehaviour
+{
+    // Variables
+    public float currentHealth; public float maxHealth; public Slider healthBar; // Health attributes  
+    public float currentMana; public float maxMana; public Slider manaBar;// Mana attributes 
+    public float currentStamina; public float maxStamina; public Slider staminaBar;// Stamina attributes  
+    public float regenTime; public float tempTime; // Regeneration time 
+
+    // Scripts and foreign variables
+    public Player_Basic_Move controlScript;
+
+    //Initialization
+    void Start()
+    {
+        regenTime = 1; // time to regenerate stats (one increment)
+        tempTime = regenTime;
+        maxHealth = 80;
+        maxMana = 80;
+        maxStamina = 300;
+        currentHealth = maxHealth;
+        currentMana = maxMana;
+        currentStamina = maxStamina;
+        DisplayStats();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Script setters and updaters
+        controlScript = this.GetComponent<Player_Basic_Move>(); // Finding the script called Control   
+
+        DisplayStats();
+
+    }
+
+    public void Regeneration(float healthRegen, float staminaRegen, float manaRegen, bool active)
+    {
+        if (active)
+        {
+            if ((tempTime -= Time.deltaTime) <= 0)
+            {
+                AlterStats(healthRegen, staminaRegen, manaRegen);
+                tempTime = regenTime;
+            }
+        }
+    }
+
+    void DisplayStats()
+    {
+        healthBar.value = currentHealth / maxHealth;
+        manaBar.value = currentMana / maxMana;
+        staminaBar.value = currentStamina / maxStamina;
+    }
+
+    public void AlterStats(float healthChange, float staminaChange, float manaChange)
+    {
+
+        // Health change
+        if (currentHealth + healthChange > maxHealth)
+        { // allows current stat to be set to max value if the current stat ends in an odd number
+            currentHealth = maxHealth;
+        }
+        if (currentHealth + healthChange < 0)
+        {  // allows current stat to be set to zero if the current stat ends in an odd number
+            currentHealth = 0;
+        }
+        if (currentHealth + healthChange <= maxHealth && currentHealth + healthChange >= 0)
+        { //makes sure current stat plus change stat cant go beyond max stat or below zero
+            currentHealth += healthChange; // applies change value to the current stat
+        }
+
+        // Mana change 
+        if (currentMana + manaChange > maxMana)
+        {
+            currentMana = maxMana;
+        }
+        if (currentMana + manaChange < 0)
+        {
+            currentMana = 0;
+        }
+        if (currentMana + manaChange <= maxMana && currentMana + manaChange >= 0)
+        {
+            currentMana += manaChange;
+        }
+
+        // Stamina change 
+        if (currentStamina + staminaChange > maxStamina)
+        {
+            currentStamina = maxStamina;
+        }
+        if (currentStamina + staminaChange < 0)
+        {
+            currentStamina = 0;
+        }
+        if (currentStamina + staminaChange <= maxStamina && currentStamina + staminaChange >= 0)
+        {
+            currentStamina += staminaChange;
+        }
+
+
+    }
+
+
+}
