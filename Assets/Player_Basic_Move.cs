699e88e960f2c30c89105edae3c3af682480191c
kylecandlin <kylecandlin@users.noreply.github.com>
2018-10-22 16:25:05 +0100

    Fixed First Jump
    Double jump needs looking at but the first jump works

--- Assets/Player_Basic_Move.cs
+++ Assets/Player_Basic_Move.cs
@@ -13,18 +13,22 @@ public class Player_Basic_Move : MonoBehaviour
     public float moveSpeed;
     public float walkSpeed;
     public float runSpeedMultiplier;
+    public float jumpForce;
+    float y;
+    public bool canDoubleJump;
+
+    public LayerMask groundLayer;
 
     private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
-
-    public float jumpSpeed;
   
     // Use this for initialization
     void Start()
     {
+        
         //Get and store a reference to the Rigidbody2D component so that we can access it.
         rb2d = GetComponent<Rigidbody2D>();
         moveSpeed = walkSpeed;
-        jumpSpeed = 30;
+        jumpForce = 300;
         walkSpeed = 4f;
         runSpeedMultiplier = 2f;
     }
@@ -32,22 +36,33 @@ public class Player_Basic_Move : MonoBehaviour
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void Update()
     {
+        // Update Foreign Scripts and Variables
         Player_Stats = this.GetComponent<Player_Stats>();
         Player_Stats_CurrentStamina = Player_Stats.currentStamina;
+
         Controls();
         Player_Stats.Regeneration(2, 10, 0, true);
         Walk();
+        Jump();
+    }
+    bool IsGrounded()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = 0.4f;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+
+        return false;
     }
 
     void Walk()
     {
-        float y = 0;
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            y += jumpSpeed;
-        }
-
+       
         if (Input.GetKey(KeyCode.LeftShift) && Player_Stats_CurrentStamina > 0f)
         {
             Player_Stats.AlterStats(0, -1, 0);
@@ -61,12 +76,42 @@ public class Player_Basic_Move : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
 
     
-        Vector2 movement = new Vector2(moveHorizontal * moveSpeed, y);
+        Vector2 movement_x = new Vector2(moveHorizontal * moveSpeed, 0);
+        Vector2 movement_y = new Vector2(0,y);
 
     
-        rb2d.position += movement * Time.deltaTime;
+        rb2d.position += movement_x * Time.deltaTime;
+        rb2d.AddForce(movement_y);
 
     }
+
+    void Jump() {
+        y = 0;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (IsGrounded())
+            {
+                y += jumpForce;
+                canDoubleJump = true;
+                
+            }
+            else {
+                if (canDoubleJump) {
+                   
+                    canDoubleJump = false;
+                    y += jumpForce;
+                    
+                }
+            }
+            
+            
+            
+
+        }
+
+    }
+
     void Controls()
     {
         if (Input.GetKeyDown(KeyCode.X))
@@ -75,4 +120,6 @@ public class Player_Basic_Move : MonoBehaviour
         }
 
     }
+
+    
 }
