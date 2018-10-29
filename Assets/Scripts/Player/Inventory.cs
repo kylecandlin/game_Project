e8a8a765ccee634aa55e9afe393048e09d489c63
kylecandlin <kylecandlin@users.noreply.github.com>
2018-10-29 12:45:15 +0000

    Added Inventory Tabs

---
+++ Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+    public GameObject InventoryPanel;
+    private bool displaying;
+
+	// Use this for initialization
+	void Start () {
+        InventoryPanel.SetActive(false);
+}
+	
+	// Update is called once per frame
+	void Update () {
+        InventoryShow();
+	}
+
+    // Shows the inventory interface
+    void InventoryShow() {
+        if (Input.GetKeyDown("tab")) {
+            displaying = !displaying;
+            InventoryPanel.SetActive(displaying);
+        }
+    }
+}
