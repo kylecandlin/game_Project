d50fca80a6f653e893a22b30341f3ef60376bac4
abbn117 <43581122+abbn117@users.noreply.github.com>
2018-10-29 14:16:15 +0000

    Inventory tabs added

--- Assets/Scripts/Player/Inventory.cs
+++ Assets/Scripts/Player/Inventory.cs
@@ -4,24 +4,52 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour {
+    public GameObject Tabs;
+    public GameObject Panels;
+    public GameObject InventoryTab;
+    public GameObject PlayerTab;
     public GameObject InventoryPanel;
+    public GameObject PlayerPanel;
+    public GameObject InventoryBackground;
+    private Camera cam;
     private bool displaying;
 
 	// Use this for initialization
 	void Start () {
-        InventoryPanel.SetActive(false);
-}
+        cam = Camera.main;
+        Tabs.SetActive(false);
+        Panels.SetActive(false);
+        InventoryPanel.SetActive(true);
+        PlayerPanel.SetActive(false);
+        InventoryBackground.SetActive(false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        InventoryShow();
+        InventoryToggle();
 	}
 
     // Shows the inventory interface
-    void InventoryShow() {
+    void InventoryToggle() {
         if (Input.GetKeyDown("tab")) {
             displaying = !displaying;
-            InventoryPanel.SetActive(displaying);
+            InventoryBackground.SetActive(displaying);
+            Tabs.SetActive(displaying);
+            Panels.SetActive(displaying);
         }
+    }
+  
+    public void PanelSelect(int page) {
+        switch (page) {
+            case 0:
+                InventoryPanel.SetActive(true);
+                PlayerPanel.SetActive(false);
+                break;
+            case 1:
+                InventoryPanel.SetActive(false);
+                PlayerPanel.SetActive(true);
+                break;
+        }
+
     }
 }
