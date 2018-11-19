3b53b1b4d039fb6bf631e8d1b715c870bc7ef280
TobyParsons <TobyParsons@users.noreply.github.com>
2018-11-19 12:26:31 +0000

    Update

--- Assets/Scripts/Player/Player_Stats.cs
+++ Assets/Scripts/Player/Player_Stats.cs
@@ -10,15 +10,7 @@ public class Player_Stats : MonoBehaviour
     public float currentHealth; public float maxHealth; public Slider healthBar; public Slider healthBarInventory; // Health attributes   
     public float currentMana; public float maxMana; public Slider manaBar; public Slider manaBarInventory;// Mana attributes 
     public float currentStamina; public float maxStamina; public Slider staminaBar; public Slider staminaBarInventory;// Stamina attributes  
-    public float regenTime; public float tempTime; // Regeneration time
-
-
-
-
-
-
-    public string inputUsername; public string inputPassword; public int inputHealth; public int inputStamina; public int inputMana; public int inputMaxHealth; public int inputMaxStamina; public int inputMaxMana;
-    string playerInsert_Link = "http://part1-17.wbs.uni.worc.ac.uk/ProjectApp/InsertUser.php";
+    public float regenTime; public float tempTime; // Regeneration time 
 
     // Scripts and foreign variables
     public Player_Basic_Move controlScript;
@@ -43,10 +35,6 @@ public class Player_Stats : MonoBehaviour
         // Script setters and updaters
         controlScript = this.GetComponent<Player_Basic_Move>(); // Finding the script called Control   
         DisplayStats();
-
-        if(Input.GetKeyDown(KeyCode.P)){
-            InsertData(inputUsername, inputPassword, inputHealth, inputStamina, inputMana, inputMaxHealth, inputMaxStamina, inputMaxMana);
-        }
 
     }
 
@@ -112,22 +100,6 @@ public class Player_Stats : MonoBehaviour
         {
             currentStamina += staminaChange;
         }
-
-
-    }
-
-    public void InsertData(string playerUsername, string playerPassword, int testPlayerHealth, int testPlayerStamina, int testPlayerMana, int testPlayerMaxHealth, int testPlayerMaxStamina, int testPlayerMaxMana) {
-        WWWForm insertForm = new WWWForm();
-        insertForm.AddField("usernamePOST", playerUsername);
-        insertForm.AddField("passwordPOST", playerPassword);
-        insertForm.AddField("healthPOST", testPlayerHealth);
-        insertForm.AddField("staminaPOST", testPlayerStamina);
-        insertForm.AddField("manaPOST", testPlayerMana);
-        insertForm.AddField("maxHealthPOST", testPlayerMaxHealth);
-        insertForm.AddField("maxStaminaPOST", testPlayerMaxStamina);
-        insertForm.AddField("maxManaPOST", testPlayerMaxMana);
-
-        WWW filePush = new WWW(playerInsert_Link, insertForm);
 
 
     }
